bc16dea78ea1bea75b4a561fb6230ce03281e30b
June <54323451+Junie88@users.noreply.github.com>
2021-08-03 18:35:07 +0800

    Update app.js

--- app.js
+++ app.js
@@ -10,20 +10,20 @@ window.onload = function() {
     scale: 1 , 
     transformOrigin: 'center center', 
     stagger: 0.1  });
-    tl.to(".image",{height:0});
-    tl.to(".text",{autoAlpha:0 });
+    tl.to(".image",.5,{ease: Power1.out,
+        stagger: 1.5});
 
     // second page
     tl.from(".container2",{autoAlpha:0})
     tl.from(".product",{ duration: 0.75,
         y: 10, autoAlpha: 0, ease: Power3.out,
         stagger: 1.5})
-    tl.from(".cta",{ duration: 0.75,
+    tl.from(".button",{ duration: 0.75,
         y: 5, autoAlpha: 0, ease: Power3.out,
         stagger: 1.5})
     tl.to(".text3",1,{ autoAlpha: 0})
     tl.to(".text2",1,{ autoAlpha: 1})
-    tl.to(".cta",1,{autoAlpha:1})
+    tl.to(".button",1,{autoAlpha:1})
     tl.to(".product",3,{ autoAlpha: 1})
     tl.to(".text2",1,{ autoAlpha: 0})
     tl.to(".text3",0.3,{ autoAlpha: 1})
